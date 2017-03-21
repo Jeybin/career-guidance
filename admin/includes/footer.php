335a20eff7093253bfc07b256f1fb736d5db7b3a
Jeybin <jeybin@gmail.com>
2017-03-21 12:45:18 +0530

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,16 @@
+</div>
+</div>
+</div>
+
+<footer>
+		 <div class="container">
+
+				<div class="copy text-center">
+					 Copyright 2017 <a href='#'>Website</a>
+				</div>
+
+		 </div>
+	</footer>
+
+</body>
+</html>
